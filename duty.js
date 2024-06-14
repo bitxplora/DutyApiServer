79823f4a03a2aac74175ba34d61f1792914c023b
Dayo Olutayo <olutayo@olutayo.com>
2024-06-14 18:26:48 +0100

    Add nodemon and update request route
    Added nodemon as dev dependency to facilitating development by providing
    automatic restarting of the server. Also added the 'update', a get route, that returns json of the last update of the database.

--- duty.js
+++ duty.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.json);
+app.get('/update', (req, res) => {
+  res.json({last: '2024-05-21:16.388'})
+})
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`)
