0ee124b1d8f181fae7ff4dace32183471ec7900c
Dayo Olutayo <olutayo@olutayo.com>
2024-06-15 18:27:21 +0100

    Refactor the database connection parameter to variable
    Refactored the database connection to use dbSource variable instead of a
    hardcoded database file path. This make it easier to change the database
    connection string without mistakenly affecting other path it.

--- data/data.js
+++ data/data.js
@@ -1,12 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
 
-const dbSource = 'tariff.db';
-const dbService = new sqlite3.Database('./tariff.db', sqlite3.OPEN_READONLY, (err) => {
+const dbSource = './tariff.db';
+const dbService = new sqlite3.Database(dbSource, sqlite3.OPEN_READONLY, (err) => {
   if (err) {
     return console.error(err.message);
   }
   console.log('Connected');
-}); 
+});
 
 
 let updateQuery = `SELECT last FROM update_t`;
