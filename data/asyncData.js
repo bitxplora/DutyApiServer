78af7c393360167bb612e27b39dfbb8d7abbb4bf
Dayo Olutayo <olutayo@olutayo.com>
2024-06-17 14:20:47 +0100

    Refactor data retrieval from pure callback to method that returned promise.
    Refactored data retrieval method from sqlite3 to returned promise
    instead of callback. This made it easy for the API route handler to
    await and use the result.

---
+++ data/asyncData.js
@@ -0,0 +1,40 @@
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+class DbService {
+
+  dbService;
+  dbSource = path.resolve(__dirname, "tariff.db");
+
+
+  constructor() {
+    this.dbService = new sqlite3.Database(this.dbSource, sqlite3.OPEN_READONLY, (err) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      // console.log('Connected');
+    });
+  }
+
+  update() {
+    return new Promise((resolve, reject) =>  {
+
+      const updateQuery = `SELECT last FROM update_t`;
+
+      this.dbService.get(updateQuery, (err, updateResult) => {
+        if (err) {
+          reject(err);
+        }
+          resolve(updateResult);
+      });
+    });
+  }
+
+
+}
+
+// const db = new DbService();
+// db.update();
+
+
+module.exports = DbService;
