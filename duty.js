839fe27f0b202ed74554efcbc4c093129fc9a7e4
Dayo Olutayo <olutayo@olutayo.com>
2024-06-20 10:06:30 +0100

    Add compression module to the app
    Added compression nodejs module to the app, this provides compression
    on all responses.

--- duty.js
+++ duty.js
@@ -1,13 +1,14 @@
 const express = require('express');
-
-const app = express();
-require('dotenv').config();
-
-
-const port = process.env.PORT || 3000;
+const compression = require('compression');
 
 const ngnRouter = require('./routes/ngn');
 
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 3000;
+
+app.use(compression());
 app.use('/', ngnRouter);
 
 
