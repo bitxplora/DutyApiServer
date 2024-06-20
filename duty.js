d27be6643f7d4a407268443abca7e3d2c3a3bfe5
Dayo Olutayo <olutayo@olutayo.com>
2024-06-20 08:57:10 +0100

    Refactor duty.js to import and use Router only
    Moved route handlers to Router file, then import and use it. This makes the
    duty.js code cleaner, and makes it easy to trace things to a particular
    file.

--- duty.js
+++ duty.js
@@ -1,38 +1,16 @@
 const express = require('express');
 
+const app = express();
 require('dotenv').config();
 
-const DbService = require('./data/asyncData');
-const db = new DbService();
 
-const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/update', async (req, res) => {
-    const data = await db.update();
-    res.json(data);
-  });
+const ngnRouter = require('./routes/ngn');
 
-app.get('/searches/:term', async (req, res) => {
-    const term = req.params.term;
-    const data = await db.search(term);
-    res.json(data);
-  });
-
-app.get('/exchanges/:exchangeCode', async (req, res) => {
-    const exchangeCode = req.params.exchangeCode;
-    const data = await db.exchangeRate(exchangeCode);
-    res.json(data);
-  });
-
-app.get('/items/:cetcode', async (req, res) => {
-    const cetcode = req.params.cetcode;
-    const data = await db.getCetCode(cetcode);
-    res.json(data);
-  });
-
+app.use('/', ngnRouter);
 
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 });
