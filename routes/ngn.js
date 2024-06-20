e84f28a48d5b6dec80d5a3666bf79e91d23d2fba
Dayo Olutayo <olutayo@olutayo.com>
2024-06-20 08:40:26 +0100

    Refactor the routes into a seperate file named 'ngn.js'
    Refactored all routes pertaining to calculating duty for Nigeria (NGN)
      into a seperate route's file that is named ngn.js inside routes directory. This
      set the foundation for preparing the app for future, to support
      multi-contries import duty calculation.

---
+++ routes/ngn.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ngnRouter = express.Router();
+
+const DbService = require('../data/asyncData');
+const db = new DbService();
+
+ngnRouter.get('/update', async (req, res) => {
+    const data = await db.update();
+    res.json(data);
+  });
+
+ngnRouter.get('/searches/:term', async (req, res) => {
+    const term = req.params.term;
+    const data = await db.search(term);
+    res.json(data);
+  });
+
+ngnRouter.get('/exchanges/:exchangeCode', async (req, res) => {
+    const exchangeCode = req.params.exchangeCode;
+    const data = await db.exchangeRate(exchangeCode);
+    res.json(data);
+  });
+
+ngnRouter.get('/items/:cetcode', async (req, res) => {
+    const cetcode = req.params.cetcode;
+    const data = await db.getCetCode(cetcode);
+    res.json(data);
+  });
+
+
+module.exports = ngnRouter;
