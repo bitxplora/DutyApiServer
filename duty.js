f02b92643828c9aeb1e82dc00a741d6979a41a3a
Dayo Olutayo <olutayo@olutayo.com>
2024-07-31 19:55:51 +0100

    Added https, cors and configured it

--- duty.js
+++ duty.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const https = require('https');
+const fs = require('fs');
+
+const cors = require('cors');
 const compression = require('compression');
 
 const ngnRouter = require('./routes/ngn');
@@ -8,10 +12,18 @@ const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
 
+const keyFile = fs.readFileSync(__dirname + "/certs/duti.key", 'utf-8');
+const certFile = fs.readFileSync(__dirname + "/certs/duti.cert", 'utf-8');
+
+const options = {key: keyFile, cert: certFile};
+
+app.use(cors({origin: '*'}));
 app.use(compression());
+
 app.use('/ngn', ngnRouter);
 
-
-app.listen(port, '0.0.0.0', () => {
+https
+  .createServer(options, app)
+  .listen(port, '0.0.0.0', () => {
   console.log(`Server running on port ${port}`);
 });
